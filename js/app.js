1796c871903d84e9af33b982a31b7a8ec71a89ee
Angel Lantigua <ncorecarbon@gmail.com>
2016-01-05 01:04:51 -0400

    pulling data and presenting to html

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// API KEYS
+var TOP_STORIES_KEY = 'ee9ad8e3cc5eaf799eafcfe1151ed010:10:73903844';
+
+//Root URL
+var URL_ROOT = 'http://api.nytimes.com/svc/';
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,62 @@
-$(document).ready(function(){
-	$('#news').text('Hey');
+$(document).ready(function() {
+	//Model Definition
+	var HomeArticle = Backbone.Model.extend({
+	});
+
+	//Collection Definition
+	var HomeArticleList	= Backbone.Collection.extend({
+		model: HomeArticle,
+		url: URL_ROOT +'topstories/v1/home.json?api-key='+ TOP_STORIES_KEY,
+		parse: function( data, options) {
+			var homeArticles = [];
+			_.each(data.results, function(article) {
+				var homeArticle = {};
+				homeArticle.section = article.section;
+				homeArticle.title = article.title;
+				homeArticle.url = article.url;
+				homeArticle.author = article.byline;
+
+				if (article.multimedia !== undefined && article.multimedia !== "") {
+					homeArticle.imgUrl = article.multimedia[1].url;
+				} else {
+					homeArticle.imgUrl = 'img/default-news-img.png';
+				}
+
+				homeArticles.push(homeArticle);
+			});
+
+			return homeArticles;
+		}
+	});
+
+	//View
+	var HomeArticlesView = Backbone.View.extend({
+		articles: undefined,
+		el: '#app-container',
+
+		initialize: function() {
+			var self = this;
+			var HomeArticles = new HomeArticleList();
+			HomeArticles.fetch({
+				success: function(collection, response, options) {
+					self.articles = collection.models;
+					self.render();					
+				},
+
+				error: function(collection, response, options) {
+
+				}
+			});
+		},
+
+		render: function() {
+			var source = $('#stories').html();
+			var template = Handlebars.compile(source);
+			var html = template({articles: this.articles});
+			this.$el.html(html);
+		}
+	});
+
+	// Kick off the App
+	var homeArticleView = new HomeArticlesView();
 });
